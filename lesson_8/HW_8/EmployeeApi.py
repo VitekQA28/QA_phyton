1edf748dea4ac885a1a66b7dc12f370fe540e150
Viktor Budnik <v.budnik@citrus-soft.ru>
2023-08-30 14:12:35 +0300

    HW_8
    Домашняя работа

---
+++ lesson_8/HW_8/EmployeeApi.py
@@ -0,0 +1,130 @@
+import requests
+import random
+from faker import Faker
+import string
+
+class EmployeeApi:
+  
+
+    def __init__(self, url):
+        self.url = url
+
+    def get_company_list(self, params_to_add = None):
+        resp = requests.get(self.url +'/company', params= params_to_add) 
+        return resp.json()
+ 
+    def get_token(self, user = 'leonardo', password = 'leads' ):    
+        creds = {
+            'username' : user,
+            'password' : password
+        }
+        resp = requests.post(self.url+'/auth/login', json=creds) 
+        return resp.json()["userToken"]
+    
+    def generate_random_words(self, num_words):  
+        fake = Faker()
+        words = [fake.word() for _ in range(num_words)]
+        return ' '.join(words)
+
+    def generate_random_email(self):
+        random_string = ''.join(random.choices(string.ascii_lowercase + string.digits, k=10))
+        email = f"{random_string}@example.com"
+        return email
+
+    def generate_random_phone(self):
+        phone = '+7' + ''.join(random.choices(string.digits, k=10))
+        return phone
+    
+    def generate_random_url(self):
+        random_string = ''.join(random.choices(string.ascii_lowercase + string.digits, k=10))
+        url = f"https://example.com/{random_string}"
+        return url
+    
+    def generate_random_birthdate(self):
+        day = random.randint(1, 28)  # выбираем случайный день от 1 до 28
+        month = random.randint(1, 12)  # выбираем случайный месяц от 1 до 12
+        year = random.randint(1900, 2022)  # выбираем случайный год от 1900 до 2022
+        birthdate = f"{year}-{month:02d}-{day:02d}"
+        return birthdate
+
+    def create_company(self, name = None, description = None):
+        if name is None:
+            name = self.generate_random_words(random.randint(1, 2))
+        if description is None:
+            description = self.generate_random_words(random.randint(2, 2))
+
+        company = {
+            'name': name,
+            'description': description
+         }
+        my_headers ={}
+        my_headers['x-client-token'] = self.get_token() 
+        resp = requests.post(self.url+'/company', json=company, headers=my_headers)
+        return resp.json()
+
+    def get_company_id(self, id):
+        resp = requests.get(self.url +'/company/'+str(id))
+        return resp.json() 
+    
+    def get_employee_list(self, company_id):
+        resp = requests.get(self.url + '/employee', params={'company': company_id})
+        return resp.json()
+    
+    def get_employee_id(self, id):
+        resp = requests.get(self.url +'/employee/'+str(id))
+        return resp.json() 
+    
+    def edit_employee(self, new_id, lastName, email, url, phone, isActive):
+        my_headers ={}
+        my_headers['x-client-token'] = self.get_token()
+        employee = {
+            "lastName": lastName,
+            "email": email,
+            "url": url,
+            "phone": phone,
+            "isActive": True
+        }
+        resp = requests.patch(self.url +'/employee/'+str(new_id), headers=my_headers, json=employee)
+        return resp.json()   
+     
+    '''
+    def create_new_employee(self, isActive, new_id, companyId, firstName =None, lastName=None, middleName=None, email=None, url=None, phone=None, birthdate=None):
+        if firstName is None:
+            firstName = self.generate_random_words(random.randint(1, 1))
+        if lastName is None:
+            lastName = self.generate_random_words(random.randint(1, 1))
+        if middleName is None:
+            middleName = self.generate_random_words(random.randint(1, 1))
+        if email is None:
+            email = self.generate_random_email()
+        if url is None:
+            url = self.generate_random_url()
+        if phone is None:
+            phone = self.generate_random_phone()
+        if birthdate is None:
+            birthdate = self.generate_random_birthdate()
+    '''       
+    
+    def create_new_employee(self, isActive, new_id, companyId, firstName, lastName, middleName, email, url, phone, birthdate):  
+        employee = {
+            "id": new_id,
+            "firstName": firstName,
+            "lastName": lastName,
+            "middleName": middleName,
+            "companyId": companyId,
+            "email": email,
+            "url": url,
+            "phone": phone,
+            "birthdate": birthdate,
+            "isActive": True
+        }
+        my_headers ={}
+        my_headers['x-client-token'] = self.get_token() 
+        resp = requests.post(self.url+'/employee', json=employee, headers=my_headers)
+        return resp.json()
+
+    def delete(self, id):
+        my_headers ={}
+        my_headers['x-client-token'] = self.get_token()
+        resp = requests.get(self.url +'/company/delete/'+str(id), headers=my_headers)
+        return resp.json()
---
+++ lesson_8/HW_8/test_employee.py
@@ -0,0 +1,168 @@
+import random
+from EmployeeApi import EmployeeApi
+import time
+import pytest
+
+
+api = EmployeeApi('https://x-clients-be.onrender.com')
+
+    #1) Получить список сотрудников
+@pytest.mark.positive_test
+def test_get_all_employee_in_new_company(): 
+    result = api.create_company()            #Создаём компанию
+    company_id = result['id']                #Записываем в переменную ID компании
+    list = api.get_employee_list(company_id) #Получить список сотрудников у компании company_id
+    emp_before = len(list)                   #Cохраняем в переменную emp_before размер списка сотрудников
+    new_id = ['id']
+    #2) Добавить нового сотрудника                              
+    firstName = api.generate_random_words(random.randint(1, 1)) 
+    lastName = api.generate_random_words(random.randint(1, 1))
+    middleName = api.generate_random_words(random.randint(1, 1))
+    email = '123@123mail.com'
+    companyId = company_id
+    url = 'https://storage.yandexcloud.net/yandexpro/storage/images/originals/qZEALznNzzFc0pO7igFzH1qNsrhvp2pOpvNjVzcv.jpg'
+    phone = '+79465431321'
+    birthdate = '01.01.1990'
+    isActive =  True
+    result = api.create_new_employee(isActive, new_id, companyId, firstName, lastName, middleName, email, url, phone, birthdate) 
+    emp_after = result['id']                 #Сохраняем ID нового сотрудника в переменную emp_after
+    #Заросить ID нового сотрудника
+    new_employee = api.get_employee_id(emp_after)  
+    #3)Изменить данные сотрудника
+    new_lastName = api.generate_random_words(random.randint(1, 1)) 
+    new_email = 'abc@mail.com'
+    new_url = ""
+    new_phone = '+79465431333'
+    isActive =  True
+    edited = api.edit_employee(emp_after, new_lastName, new_email, new_url, new_phone, isActive) 
+    
+    assert edited['email'] == new_email     #Проверить, что изменился email
+    assert edited['url'] == new_url         #Проверить, что изменился URL
+    assert edited['isActive'] == True       #Проверить, что сотрудник активен
+    assert new_employee['id'] == emp_after  #Проверить, что мы получили ID именно нового сотрудника
+    assert emp_before == 0                  #Проверить кол-во сотрудников в компании до создания нового.
+    assert emp_before < emp_after           #Проверить, что список сотрудников увеличился
+    delete = api.delete(company_id)         #Удалить компанию после теста
+
+
+ 
+#1) Получить список сотрудников
+@pytest.mark.xfail(strict=True)
+@pytest.mark.negative_test
+def test_get_all_employee_in_new_company_negative(): 
+    result = api.create_company()            #Создаём компанию
+    company_id = result['id']                #Записываем в переменную ID компании
+    list = api.get_employee_list(company_id) #Получить список сотрудников у компании company_id
+    emp_before = len(list)                   #Cохраняем в переменную emp_before размер списка сотрудников
+    new_id = ['id']
+    #2) Добавить нового сотрудника                              
+    firstName = api.generate_random_words(random.randint(1, 1)) 
+    lastName = api.generate_random_words(random.randint(1, 1))
+    middleName = api.generate_random_words(random.randint(1, 1))
+    email = '123@123mail.com'
+    companyId = company_id
+    url = 'https://storage.yandexcloud.net/yandexpro/storage/images/originals/qZEALznNzzFc0pO7igFzH1qNsrhvp2pOpvNjVzcv.jpg'
+    phone = '+79465431321'
+    birthdate = '01.01.1990'
+    isActive =  True
+    result = api.create_new_employee(isActive, new_id, companyId, firstName, lastName, middleName, email, url, phone, birthdate) 
+    emp_after = result['id']                 #Сохраняем ID нового сотрудника в переменную emp_after
+    #Заросить ID нового сотрудника
+    new_employee = api.get_employee_id(emp_after)  
+    #3)Изменить данные сотрудника
+    new_lastName = api.generate_random_words(random.randint(1, 1)) 
+    #new_email = 'abc@mail.com'
+    new_url = ""
+    new_phone = '+79465431333'
+    isActive =  True
+    edited = api.edit_employee(emp_after, new_lastName, new_email, new_url, new_phone, isActive) 
+    
+    assert edited['email'] == new_email     #Не передаём один из параметров
+    assert edited['url'] == new_url         #Проверить, что изменился URL
+    assert edited['isActive'] == True       #Проверить, что сотрудник активен
+    assert new_employee['id'] == emp_after  #Проверить, что мы получили ID именно нового сотрудника
+    assert emp_before == 0                  #Проверить кол-во сотрудников в компании до создания нового.
+    assert emp_before == emp_after          #Проверить, что список остался неизменным
+    delete = api.delete(company_id)         #Удалить компанию после теста
+
+
+#Задокументировал изначальный вариант тестов, но они работают только по отдельности, 
+#если запускать их разом - идет конфликт и всё падает.
+'''
+#2) Добавить нового сотрудника 
+def test_add_new_employee():
+    result = api.create_company()
+    company_id = result['id']   
+    new_id = ['id']
+    firstName = api.generate_random_words(random.randint(1, 1))
+    lastName = api.generate_random_words(random.randint(1, 1))
+    middleName = api.generate_random_words(random.randint(1, 1))
+    email = '123@123mail.com'
+    companyId = company_id
+    url = 'https://storage.yandexcloud.net/yandexpro/storage/images/originals/qZEALznNzzFc0pO7igFzH1qNsrhvp2pOpvNjVzcv.jpg'
+    phone = '+79465431321'
+    birthdate = '01.01.1990'
+    isActive =  True
+    result = api.create_new_employee(isActive, new_id, companyId, firstName, lastName, middleName, email, url, phone, birthdate)
+    emp_id = result['id']
+    assert emp_id != 0 #Проверить что создался новый пользователь и его ID не равно 0
+    time.sleep(1)
+    delete = api.delete(company_id)
+
+#3) Получить сотрудника по ID
+
+def test_get_employee_id():
+    result = api.create_company()
+    company_id = result['id']  
+    new_id = ['id']
+    firstName = api.generate_random_words(random.randint(1, 1))
+    lastName = api.generate_random_words(random.randint(1, 1))
+    middleName = api.generate_random_words(random.randint(1, 1))
+    email = '123@mail.com'
+    companyId = company_id
+    url = 'https://storage.yandexcloud.net/yandexpro/storage/images/originals/qZEALznNzzFc0pO7igFzH1qNsrhvp2pOpvNjVzcv.jpg'
+    phone = '+79465431321'
+    birthdate = '01.01.1990'
+    isActive =  True
+    #result = api.create_new_employee(new_id, companyId, isActive)
+    result = api.create_new_employee(new_id, firstName, lastName, middleName, companyId, email, url, phone, birthdate, isActive)
+    emp_id = result['id']
+    new_employee = api.get_employee_id(emp_id)  #Заросить ID нового сотрудника
+    assert new_employee['id'] == emp_id         #Проверить что вызванный id сотрудника сотрудника равен созданому новому.
+    time.sleep(1)
+    delete = api.delete(company_id)
+     
+#4) Изменить информацию о сотруднике.
+
+def test_edit_employee():  
+    result = api.create_company()
+    company_id = result['id']  
+    new_id = ['id']
+    firstName = api.generate_random_words(random.randint(1, 1))
+    lastName = api.generate_random_words(random.randint(1, 1))
+    middleName = api.generate_random_words(random.randint(1, 1))
+    email = '123@mail.com'
+    companyId = company_id
+    url = 'https://storage.yandexcloud.net/yandexpro/storage/images/originals/qZEALznNzzFc0pO7igFzH1qNsrhvp2pOpvNjVzcv.jpg'
+    phone = '+79465431321'
+    birthdate = '01.01.1990'
+    isActive =  True
+    result = api.create_new_employee(new_id, firstName, lastName, middleName, companyId, email, url, phone, birthdate, isActive)
+    emp_id = result['id'] 
+    new_lastName = api.generate_random_words(random.randint(1, 1))
+    new_email = 'abc@mail.com'
+    new_url = ""
+    new_phone = '+79465431333'
+    isActive =  True
+    edited = api.edit_employee(emp_id, new_lastName, new_email, new_url, new_phone, isActive)
+    assert edited['email'] == new_email
+    assert edited['url'] == new_url   
+    assert edited['isActive'] == True
+    time.sleep(1)
+    delete = api.delete(company_id)
+'''
+
+
+
+
+
