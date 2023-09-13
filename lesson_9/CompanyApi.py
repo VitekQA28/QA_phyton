78296660b01a26d51ce68d763047b798928ea8f6
VitekQA28 <121097565+VitekQA28@users.noreply.github.com>
2023-09-13 08:47:56 +0300

    Add files via upload

---
+++ lesson_9/CompanyApi.py
@@ -0,0 +1,61 @@
+import requests
+
+from faker import Faker
+
+class CompanyApi:
+
+    def __init__(self, url):
+        self.url = url
+
+    def get_company_list(self, params_to_add = None):
+        resp = requests.get(self.url +'/company', params= params_to_add) 
+        return resp.json()
+ 
+    def get_token(self, user = 'leonardo', password = 'leads' ):    
+        creds = {
+            'username' : user,
+            'password' : password
+        }
+        resp = requests.post(self.url+'/auth/login', json=creds) 
+        return resp.json()["userToken"]
+    
+    def generate_random_words(self, num_words):  
+        fake = Faker()
+        words = [fake.word() for _ in range(num_words)]
+        return ' '.join(words)
+
+    def create_company(self, name, description =''):
+        company = {
+            'name': name,
+         'description': description
+         }
+        my_headers ={}
+        my_headers['x-client-token'] = self.get_token() 
+        resp = requests.post(self.url+'/company', json=company, headers=my_headers)
+        return resp.json()
+
+    def get_company_id(self, id):
+        resp = requests.get(self.url +'/company/'+str(id))
+        return resp.json() 
+
+    def edit(self, new_id, new_name, new_descr):
+        my_headers ={}
+        my_headers['x-client-token'] = self.get_token()
+        company = {
+            'name': new_name,
+            'description': new_descr
+        }
+        resp = requests.patch(self.url +'/company/'+str(new_id), headers=my_headers, json=company)
+        return resp.json()
+
+    def delete(self, id):
+        my_headers ={}
+        my_headers['x-client-token'] = self.get_token()
+        resp = requests.get(self.url +'/company/delete/'+str(id), headers=my_headers)
+        return resp.json()
+    
+    def set_active_state(self, id, isActive):
+        my_headers ={}
+        my_headers['x-client-token'] = self.get_token()
+        resp = requests.patch(self.url +'/company/status/'+str(id), headers=my_headers, json={'isActive':isActive})
+        return resp.json()
