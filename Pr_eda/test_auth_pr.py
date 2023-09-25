ced1d4e7b782f36c574de58a419f835b5cd8711b
Viktor Budnik <v.budnik@citrus-soft.ru>
2023-09-25 15:22:58 +0300

    test

--- Pr_eda/test_auth_pr.py
+++ Pr_eda/test_auth_pr.py
@@ -2,17 +2,21 @@ from AuthPagePr import AuthPagePr
 import allure
 import pytest
 
-
+@allure.severity("critical")
 @allure.epic("Авторизация")
 def test_auth():
-    main_page = AuthPagePr('browser')
-    main_page.open_auth_window()
-    main_page.login_as()
-    actual_name = main_page.get_accautnt_info()
-    current_url = main_page.get_current_url()
-    with allure.step("Проверка, что URL " + current_url + "заканчивается на personal/"):
-        assert "/personal" in current_url
-    with allure.step("Проверка данных пользователя"):
-        assert actual_name == "Budnik Viktornik"
+    with allure.step("Отрываем и настраиваем браузер"):
+        main_page = AuthPagePr('browser')
+    with allure.step("Отрываем страницу авторизации"):
+        main_page.open_auth_window()
+    with allure.step("Водим валидные данные"):
+        main_page.login_as()
+    with allure.step("Проводим проверку данных"):
+        actual_name = main_page.get_accautnt_info()
+        current_url = main_page.get_current_url()
+        with allure.step("Проверка, что URL " + current_url + "заканчивается на personal/"):
+            assert "/personal" in current_url
+        with allure.step("Проверка данных пользователя"):
+            assert actual_name == "Budnik Viktornik"
     main_page.close()
     
